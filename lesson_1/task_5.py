7fd9299e3365e0b0b3b6f0cebc9bb7fdf2008961
Anna Seliukova <moonor05@gmail.com>
2023-07-09 23:30:00 +0300

    homework 1

---
+++ lesson_1/task_5.py
@@ -0,0 +1,13 @@
+# first option
+
+# print('Escape sequences\n' + '\\a Bell (alert)\n' + '\\b Backspace\n' + '\\n New line\n'
+#       + 'Horizontal tab\n' + '\\\ Backslash \\\n' + '\\"Double quotation mark"\n'
+#       + "\\'Single quotation mark'\n")
+
+#second option
+
+esc_list = ['Escape sequences', '\\a Bell (alert)', '\\b Backspace', '\\n New line', 'Horizontal tab',
+            '\\\ Backslash \\', '\\"Double quotation mark"', "\\'Single quotation mark'"]
+for phrase in esc_list:
+    print(phrase)
+
---
+++ lesson_1/task_6.py
@@ -0,0 +1,7 @@
+x = 7
+y = 4
+print("x = " + str(x) + "\n" + "y = " + str(y))
+print("x + y = " + str(x+y))
+print("x / y = " + str(x/y))
+print("x % y = " + str(x%y))
+print("x^y = " + str(x**y))
