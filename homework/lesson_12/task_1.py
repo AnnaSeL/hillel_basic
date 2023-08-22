74ec29eaf5f08f0b15f9fdfef69267bc6299b81e
Anna Seliukova <moonor05@gmail.com>
2023-08-22 20:59:06 +0300

    comments added

--- homework/lesson_12/task_1.py
+++ homework/lesson_12/task_1.py
@@ -21,11 +21,13 @@ class Street:
         return f'Number {self.number} ; Houses: {self.houses}'
 
     def add_house(self):
+        """Create new house"""
         house_num = len(self.houses) + 1
         house = House(house_num)
         return self.houses.append(house)
 
     def del_house(self, house_num: int):
+        """Delete the given house"""
         if 1 <= house_num <= len(self.houses):
             del self.houses[house_num-1]
         else:
@@ -40,6 +42,7 @@ class City:
         return f'Streets: {self.streets}'
 
     def fill_city(self, streets_amount: int):
+        """Fill city with streets, houses and people"""
         for street_num in range(1, streets_amount+1):
             street = Street(street_num)
             house_num = random.randint(5, 20)
@@ -48,11 +51,13 @@ class City:
             self.streets.append(street)
 
     def add_street(self):
+        """Create new street"""
         street_num = len(self.streets) + 1
         street = Street(street_num)
         return self.streets.append(street)
 
     def del_street(self, street_num: int):
+        """Delete the given street"""
         if 1 <= street_num <= len(self.streets):
             del self.streets[street_num-1]
         else:
@@ -60,10 +65,12 @@ class City:
 
     @property
     def population(self):
+        """Get information about population"""
         population = sum(house.population for street in self.streets for house in street.houses)
         return f'Population: {population}'
 
     def print_table(self):
+        """Print table with information about city"""
         table_data = []
         for street in self.streets:
             for house in street.houses:
@@ -72,9 +79,12 @@ class City:
         print(tabulate(table_data, headers=table_headers))
 
 
+# usage example
 if __name__ == "__main__":
     city = City()
     city.fill_city(3)
+    city.print_table()
+    city.streets[2].add_house()
     city.print_table()
     city.del_street(1)
     city.print_table()
